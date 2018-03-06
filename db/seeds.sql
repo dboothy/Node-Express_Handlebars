2c846edb613085be048d1780795be7b4ffaf797a
David Booth-Metwally <davidbmetwally@gmail.com>
2018-03-05 21:38:24 -0800

    eat-da burger with creation and boolean value change

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('gourmet burger',);
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('chicken burger');
+
+
+
+
